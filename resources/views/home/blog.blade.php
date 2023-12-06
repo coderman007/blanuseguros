1e19b6e57026d66df8bc78611592a3b871ddc161
amuna <santiyalexis32@gmail.com>
2023-12-06 13:40:09 -0500

    blog

--- resources/views/home/blog.blade.php
+++ resources/views/home/blog.blade.php
@@ -1,5 +1,175 @@
 <div>
     <x-guest-layout>
+        <div class="relative w-full h-[50vh] bg-red-400">
+            <img class="w-full object-cover h-full" src="{{ asset('images/carrusel/img2.webp') }}" alt="">
+            <div class="flex justify-center items-center absolute top-0 w-full h-full gap-x-6">
+                <div class="  bg-gray-100 w-2/5 h-[40vh] rounded-md">
+                </div>
+                <div class="bg-gray-100 w-2/5 h-[40vh] rounded-md">
+                </div>
+            </div>
+        </div>
+        <div class="my-16  flex justify-center">
+            <div class=" w-2/3">
+                <div class="flex justify-center my-3 ">
+                    <div class="w-1/3 p-4">
+                        <img class="rounded-lg" src="{{asset('images/carrusel/img4.webp')}}" alt="">
+                    </div>
+                    <div class="w-2/5 p-4">
+                        <div class="text-3xl font-extrabold mb-4">TITULO DEL ARTICULO</div>
+                        <div>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem, mollitia.
+                            Error dolores repellendus cupiditate voluptatem inventore possimus unde
+                            aliquam hic eveniet minima quos, incidunt nemo voluptate veritatis veniam
+                            facilis repudiandae.
+                        </div>
+                        <button class="bg-blue-900 rounded-md py-1 px-4 mt-8 text-gray-100">Ver más</button>
+                    </div>
+                </div>
+                <div class="flex justify-center my-3">
+                    <div class="w-1/3 p-4">
+                        <img class="rounded-lg" src="{{asset('images/lines/auto.jpeg')}}" alt="">
+                    </div>
+                    <div class="w-2/5 p-4">
+                        <div class="text-3xl font-extrabold mb-4">TITULO DEL ARTICULO</div>
+                        <div>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem, mollitia.
+                            Error dolores repellendus cupiditate voluptatem inventore possimus unde
+                            aliquam hic eveniet minima quos, incidunt nemo voluptate veritatis veniam
+                            facilis repudiandae.
+                        </div>
+                        <button class="bg-blue-900 rounded-md py-1 px-4 mt-8 text-gray-100">Ver más</button>
+                    </div>
+                </div>
+                {{-- <div>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                    <p>hola</p>
+                </div> --}}
+            </div>
+            <div class="w-1/3">
+                <div class="sticky top-16">
+                    <div class="bg-gray-200 px-4 rounded-md py-1 text-base font-semibold w-fit">
+                        Recomendaciones
+                    </div>
+                    <div class="flex m-4 gap-x-4 items-center w-3/4">
+                        <img class="w-28 rounded-lg" src="{{asset('images/lines/auto.jpeg')}}" alt="">
+                        <div>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        </div>
+                    </div>
+                    <div class="flex m-4 gap-x-4 items-center w-3/4">
+                        <img class="w-28 rounded-lg" src="{{asset('images/lines/auto.jpeg')}}" alt="">
+                        <div>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        </div>
+                    </div>
+                    <div class="flex m-4 gap-x-4 items-center w-3/4">
+                        <img class="w-28 rounded-lg" src="{{asset('images/lines/auto.jpeg')}}" alt="">
+                        <div>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <h1>Blog</h1>
     </x-guest-layout>
 </div>
